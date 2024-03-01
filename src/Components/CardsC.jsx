35c9136e0070bb2e56e47f1cd3dac60d55d0e12a
unknown <alvarosgd97@gmail.com>
2024-03-01 18:33:48 -0300

    solucion en la parte de cards

--- src/Components/CardsC.jsx
+++ src/Components/CardsC.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom"; // Asegúrate de importar Link desde react-router-dom
 import clienteAxios, { configHeaders } from "../helpers/clientAxios";
 import Swal from "sweetalert2";
 import "../css/CardsC.css";
@@ -18,33 +19,33 @@ const CardsC = ({ url, titulo, precio, codigo, idProduct, idPage, onDelete }) =>
       }
     } catch (error) {
       console.log(error);
-
     }
   };
 
   return (
-    <Card style={{ width:"18rem"}} className="mb-5">
-    <Card.Img variant="top" src={url} className="object-fit-cover" />
-    <Card.Body style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-      <Card.Title>{titulo}</Card.Title>
-      <Card.Text >{precio}</Card.Text>
-      <Card.Text style={{whiteSpace: 'nowrap', 
-  overflow: 'hidden',
-  textOverflow: 'ellipsis'}}>{codigo}</Card.Text>
-      {idPage === "FavPage" ? (
-        <Link to={`#`} className="btn btn-danger" onClick={deleteProdFav}>
-          Eliminar
-        </Link>
-      ) : (
-        <Link to={`/product/${idProduct}`} className="btn btn-cards">
-          Ver mas
-        </Link>
-      )}
-    </Card.Body>
-  </Card>
+    <Card style={{ width: "18rem" }} className="mb-5">
+      <Card.Img variant="top" src={url} className="object-fit-cover" />
+      <Card.Body style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+        <Card.Title>{titulo}</Card.Title>
+        <Card.Text>{precio}</Card.Text>
+        <Card.Text style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          {codigo}
+        </Card.Text>
+        {idPage === "FavPage" ? (
+          <Link to="#" className="btn btn-danger" onClick={deleteProdFav}>
+            Eliminar
+          </Link>
+        ) : (
+          <Link to={`/product/${idProduct}`} className="btn btn-cards">
+            Ver mas
+          </Link>
+        )}
+      </Card.Body>
+    </Card>
   );
 };
 
 export default CardsC;
+
 
 
